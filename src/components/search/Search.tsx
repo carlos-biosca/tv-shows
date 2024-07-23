257e9ddcbbbdb8745123c7816e67253335744f39
carlos-biosca <cbiosca82@gmail.com>
2024-07-23 21:27:41 +0200

    open close input results

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useState, useEffect} from 'react';
+import { ReactElement, useState, useEffect, useRef, ChangeEvent} from 'react';
 import { CiSearch } from "react-icons/ci";
 import SearchCard from '../searchcard/SearchCard';
 import './Search.scss'
@@ -8,6 +8,12 @@ const apiKey = import.meta.env.VITE_SHOWS_API_KEY
 const Search = (): ReactElement => {
   const [inputValue, setInputValue] = useState<string>('')
   const [results, setResults] = useState<Show[]>([])
+  const [open, setOpen] = useState<boolean>(false)
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setInputValue(e.target.value)
+  }
 
   useEffect(() => {
     const getSearchResults = async () => {
@@ -24,30 +30,56 @@ const Search = (): ReactElement => {
     getSearchResults()
   },[inputValue])
 
+  const handleOpen = ():void => {
+    setOpen(true);
+  };
+
+  const handleClose = ():void => {
+    setOpen(false)
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent): void => {
+      if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+        handleClose()
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [])
+
   return (     
-    <div className='search'>
-        <form className='search__form'>
-          <button 
-          className='search__button'
-          type='button'
-          >
-            <CiSearch size={20}/>
-          </button>
-          <input 
-            type="text" 
-            className='search__input'
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-        </form>
-        {
-          results.length > 0 ? (
-            <ul className='search__list'>
-              {
-                results.map(show => <SearchCard info={show} key={show.id}/>)
-              }
-            </ul> 
-          ) : null
-        }
+    <div className='search' ref={containerRef}>
+      <form className='search__form'>
+        <button 
+        className='search__button'
+        type='button'
+        onClick={handleOpen}
+        >
+          <CiSearch size={20}/>
+        </button>
+        <input 
+          type="text" 
+          className='search__input'
+          onChange={handleChange}
+          onFocus={handleOpen}
+        />
+      </form>
+      {
+        open && results.length > 0 ? (
+          <ul className='search__list'>
+            {
+              results.map(show => 
+                <li key={show.id}>
+                  <SearchCard info={show} close={handleClose} key={show.id}/>
+                </li>
+              )
+            }
+          </ul> 
+        ) : null
+      }
     </div> 
   );
 }
--- src/components/searchcard/SearchCard.tsx
+++ src/components/searchcard/SearchCard.tsx
@@ -2,21 +2,23 @@ import { ReactElement } from "react";
 import { baseUrl, smallSize } from '../../constants';
 import './SearchCard.scss'
 import { Show } from "../../vite-env";
+import { Link } from "react-router-dom";
 
 interface SearchCardProps {
-  info: Show
+  info: Show,
+  close: () => void
 }
 
-const SearchCard = ({ info }: SearchCardProps): ReactElement => {
-  const {poster_path, name, first_air_date} = info
+const SearchCard = ({ info, close }: SearchCardProps): ReactElement => {
+  const {poster_path, name, first_air_date, id} = info
   return ( 
-    <li className='searchcard'>
+    <Link to={`/details/${id}`} className='searchcard' onClick={close}>
       <img className='searchcard__image' src={`${baseUrl}${smallSize}${poster_path}`} alt="poster result" />
       <div className="searchcard__text">
           <p className="searchcard__title">{name}</p>
           <p className="searchcard__year">{first_air_date.slice(0,4)}</p>
       </div>
-    </li>
+    </Link>
   );
 }
 
