a25826a2f48e4ec10688ffc441a6a7dee5c6af27
carlos-biosca <cbiosca82@gmail.com>
2024-07-23 22:10:18 +0200

    scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar"
 import Footer from "./components/footer/Footer"
@@ -9,6 +9,7 @@ function App() {
       <Navbar />
       <Outlet />
       <Footer />
+      <ScrollRestoration />
     </>
   )
 }
